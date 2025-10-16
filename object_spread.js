a58a77e182587eec07fa8cec09427ffe4de9a426
손유승 <youseung5153@gmail.com>
2025-10-16 23:03:54 +0900

    spread 연산자

--- object_spread.js
+++ object_spread.js
@@ -1,7 +1,7 @@
 let user = { name: 'Kim', city: 'Seoul' };
 
 user = { ...user, age: 28 };
-console.log(user);
+console.log(user); // { name: 'Kim', city: 'Seoul', age: 28 }
 
 user = { ...user, name: 'John', age: 30};
-console.log(user);
+console.log(user); // { name: 'John', city: 'Seoul', age: 30 }
