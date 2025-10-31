a18b7f1c1c09d0cb53a62afc90a2e9614deaf796
손유승 <youseung5153@gmail.com>
2025-10-31 17:38:56 +0900

    map

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 var numbers = [ 1, 2, 3, 4, 5 ];
-var twice = numbers.map(function(value, index) { // map 함수를 통해서 배열의 요소에 2를 곱한 새로운 배열을 생성
+var twice = numbers.map((value, index) => { // map 함수를 통해서 배열의 요소에 2를 곱한 새로운 배열을 생성
     return value * 2;
 });
 
